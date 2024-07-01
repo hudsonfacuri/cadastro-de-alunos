db4aaf7d0a52c5f0e3eaa01560930513d8727bba
hudsonfacuri <43973756+hudsonfacuri@users.noreply.github.com>
2024-07-01 08:49:05 -0300

    Add files via upload

---
+++ objetoLiteral.js
@@ -0,0 +1,36 @@
+const user = {
+    nome: "juliana",
+    email: "j@j .com",
+    nascimento: "2021/01/01",
+    role: "admin",
+    ativo: true,
+    exibirInfos: function () {
+        console.log(this.nome, this.email)
+    }
+}
+
+const admin = {
+    nome: "Mariana",
+    email: "m@m .com",
+    role: "admin",
+    criarCurso() {
+        console.log('curso criado!')
+    }
+}
+
+Object.setPrototypeOf(admin, user)
+admin.criarCurso()
+admin.exibirInfos()
+
+/*herança de protótipo
+//user.exibirInfos()
+//const exibir = user.exibirInfos
+//exibir()
+
+const exibir = function() {
+    console.log(this.nome)
+}
+
+const exibirNome = exibir.bind(user)
+exibirNome()
+exibir()*/
